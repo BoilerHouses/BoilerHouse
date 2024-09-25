17b29df374c24804d1ee0f332a9f06c0a993b366
Pixelgost <thisisadhi@gmail.com>
2024-09-25 12:30:03 -0400

    Added bucket functionality

--- BoilerHouseBackend/api/urls.py
+++ BoilerHouseBackend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ping
+from .views import ping, tryBucket
 
 urlpatterns = [
-    path('ping/', ping, name='ping')
+    path('ping/', ping, name='ping'),
+    path('buckets/', tryBucket, name='tryBucket')
 ]
