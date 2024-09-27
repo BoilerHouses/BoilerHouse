4dde015fe9e15d223f6bd2498ae214e9c5057e6d
Pixelgost <thisisadhi@gmail.com>
2024-09-27 14:10:13 -0400

    Added registration backend account

--- BoilerHouseBackend/api/urls.py
+++ BoilerHouseBackend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ping, try_bucket, log_in, create_account
+from .views import ping, try_bucket, log_in, create_account, register_account
 
 urlpatterns = [
     path('ping/', ping, name='ping'),
     path('buckets/', try_bucket, name='tryBucket'),
     path('user/', log_in, name='getUser'),
+    path('account/', register_account, name='register_account'),
     path('user', create_account, name='createUser')
 
 ]
