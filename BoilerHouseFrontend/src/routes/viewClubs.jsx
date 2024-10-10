2ab6853ed8568bf37178576c805ccc88acd9852f
Pixelgost <thisisadhi@gmail.com>
2024-10-09 23:08:48 -0400

    Added no clubs found

--- BoilerHouseFrontend/src/routes/viewClubs.jsx
+++ BoilerHouseFrontend/src/routes/viewClubs.jsx
@@ -43,30 +43,46 @@ const ViewClubs = () => {
         fetchProfile();
       }, []);
       
-    
-      return (
-        <div className="container mx-auto p-5 max-w-3xl">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full p-3 mb-5 border border-gray-300 rounded"  
-          />
-          <div className="grid grid-cols-2 gap-5 overflow-y-auto max-h-96">
-            {filteredData.map(item => (
-              <div
-                key={item.name}
-                className="relative h-48 rounded-lg bg-cover bg-center border border-gray-600	"
-                style={{ backgroundImage: `url("${item.icon}")` }}
-              >
-                <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-white text-center p-2 rounded-b-lg">
-                  <span>{item.name}</span>
+      if (filteredData.length > 0) {
+        return (
+            <div className="container mx-auto p-5 max-w-3xl">
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-full p-3 mb-5 border border-gray-300 rounded"  
+            />
+            <div className="grid grid-cols-2 gap-5 overflow-y-auto max-h-96">
+                {filteredData.map(item => (
+                <div
+                    key={item.name}
+                    className="relative h-48 rounded-lg bg-cover bg-center border border-gray-600	"
+                    style={{ backgroundImage: `url("${item.icon}")` }}
+                >
+                    <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 text-white text-center p-2 rounded-b-lg">
+                    <span>{item.name}</span>
+                    </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      );
+                ))}
+            </div>
+            </div>
+        );
+      } else {
+        return  (
+            <div className="container mx-auto p-5 max-w-3xl">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="w-full p-3 mb-5 border border-gray-300 rounded"  
+                />
+                <div className="flex justify-center h-screen">
+                     <p className="text-black text-center font-bold rounded-md">No clubs found matching criteria</p>
+                </div>
+            </div>
+        );
+      }
     };
 export default ViewClubs;
