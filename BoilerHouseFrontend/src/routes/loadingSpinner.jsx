ad3b1946260dff537242c0e51c7cce9e040bc2fe
asawaarin0 <112415653+asawaarin0@users.noreply.github.com>
2024-10-19 18:15:05 -0400

    bug fixes

---
+++ BoilerHouseFrontend/src/routes/loadingSpinner.jsx
@@ -0,0 +1,8 @@
+
+const LoadingSpinner = () => {
+    return (
+        <div className="flex justify-center items-center animate-spin h-8 w-8 border-b-4 border-t-4 border-red-500 rounded-full"></div>
+    );
+};
+
+export default LoadingSpinner;
