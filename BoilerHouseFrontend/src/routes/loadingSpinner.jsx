f7cbc9261e8904dc052db81197e33b67fd16b182
asawaarin0 <112415653+asawaarin0@users.noreply.github.com>
2024-10-20 00:58:25 -0400

    finished send mass email feature

--- BoilerHouseFrontend/src/routes/loadingSpinner.jsx
+++ BoilerHouseFrontend/src/routes/loadingSpinner.jsx
@@ -1,7 +1,10 @@
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ message }) => {
     return (
-        <div className="flex justify-center items-center animate-spin h-8 w-8 border-b-4 border-t-4 border-red-500 rounded-full"></div>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="flex justify-center items-center animate-spin h-8 w-8 border-b-4 border-t-4 border-red-500 rounded-full"></div>
+            {message && <p className="mt-4 text-center">{message}</p>}
+        </div>
     );
 };
 
